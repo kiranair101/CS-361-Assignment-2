b71f2c34110e90f1793b9d73c5e71b320b312e25
elikurlbaum <elikurlbaum@icloud.com>
2023-01-23 19:52:43 -0600

    Stopping point after 1/23.

--- project/src/components/Search.js
+++ project/src/components/Search.js
@@ -20,30 +20,42 @@ export default function Search(){
         })
     }
 
+    const displayData = async (data, index) => {
+        setValue("name", data['data'][index]['name'])
+        setValue("type", data['data'][index]['type'])
+        setValue("race", data['data'][index]['race'])
+        setValue("atk", data['data'][index]['atk'])
+        setValue("def", data['data'][index]['def'])
+        setValue("level", data['data'][index]['level'])
+        setValue("attribute", data['data'][index]['attribute'])
+        setValue("archetype", data['data'][index]['archetype'])
+    }
+
+    const displayImage = async (data, index, image_index) => {
+        document.getElementById("Card_Image").src=data['data'][index]['card_images'][image_index]['image_url']
+    }
+
     const onSubmit = async (formValues) => {
         console.log(formValues)
 
+        // Build the url from the form values. 
         let url = 'https://db.ygoprodeck.com/api/v7/cardinfo.php?'
         for (const parameter in formValues){
             if(formValues[parameter] !== ''){
                 url = url + `${parameter}=${formValues[parameter]}&`
             }
         }
-        console.log(url)
+
+        // Send the GET request to the Yu-Gi-Oh! API. 
         try{
             const response = await fetch(url)
             const data = await response.json()
-            console.log(data['data'][0]['card_images'][0]['image_url'])
-            document.getElementById("Card_Image").src=data['data'][0]['card_images'][0]['image_url']
             console.log(data)
-            setValue("name", data['data'][0]['name'])
-            setValue("type", data['data'][0]['type'])
-            setValue("race", data['data'][0]['race'])
-            setValue("atk", data['data'][0]['atk'])
-            setValue("def", data['data'][0]['def'])
-            setValue("level", data['data'][0]['level'])
-            setValue("attribute", data['data'][0]['attribute'])
-            setValue("archetype", data['data'][0]['archetype'])
+            
+            await displayImage(data, 0, 0)
+        
+            await displayData(data, 0)
+        
         } catch(error){
             console.log(error)
             alert("No card matches your input. Please adjust your search criteria.")
@@ -56,8 +68,54 @@ export default function Search(){
         <form onSubmit={handleSubmit(onSubmit)} onReset={onReset}>
             <input type="text" placeholder="Name" {...register("name")}/>
             <input type="number" placeholder="Level" {...register("level")}/>
-            <input type="text" placeholder="Type" {...register("type")}/>
-            <input type="text" placeholder="Attribute" {...register("attribute")}/>
+            <select {...register("type")}>
+                <option value="" disabled selected hidden>Type</option>
+                <optgroup label="Main Deck Types">
+                    <option value="Effect Monster">Effect Monster</option>
+                    <option value="Flip Effect Monster">Flip Effect Monster</option>
+                    <option value="Flip Tuner Monster">Flip Tuner Monster</option>
+                    <option value="Gemini Monster">Gemini Monster</option>
+                    <option value="Normal Monster">Normal Monster</option>
+                    <option value="Normal Tuner Monster">Normal Tuner Monster</option>
+                    <option value="Pendulum Effect Monster">Pendulum Effect Monster</option>
+                    <option value="Pendulum Effect Ritual Monster">Pendulum Effect Ritual Monster</option>
+                    <option value="Pendulum Flip Effect Monster">Pendulum Flip Effect Monster</option>
+                    <option value="Pendulum Normal Monster">Pendulum Normal Monster</option>
+                    <option value="Pendulum Tuner Effect Monster">Pendulum Tuner Effect Monster</option>
+                    <option value="Ritual Effect Monster">Ritual Effect Monster</option>
+                    <option value="Ritual Monster">Ritual Monster</option>
+                    <option value="Spell Card">Spell Card</option>
+                    <option value="Spirit Monster">Spirit Monster</option>
+                    <option value="Toon Monster">Toon Monster</option>
+                    <option value="Trap Card">Trap Card</option>
+                    <option value="Tuner Monster">Tuner Monster</option>
+                    <option value="Union Effect Monster">Union Effect Monster</option>
+                </optgroup>
+                <optgroup label="Extra Deck Types">
+                    <option value="Fusion Monster">Fusion Monster</option>
+                    <option value="Link Monster">Link Monster</option>
+                    <option value="Pendulum Effect Fusion Monster">Pendulum Effect Fusion Monster</option>
+                    <option value="Synchro Monster">Synchro Monster</option>
+                    <option value="Synchro Pendulum Effect Monster">Synchro Pendulum Effect Monster</option>
+                    <option value="Synchro Tuner Monster">Synchro Tuner Monster</option>
+                    <option value="XYZ Monster">XYZ Monster</option>
+                    <option value="XYZ Pendulum Effect Monster">XYZ Pendulum Effect Monster</option>
+                </optgroup>
+                <optgroup label="Other Types">
+                    <option value="Skill Card">Skill Card</option>
+                    <option value="Token">Token</option>
+                </optgroup>
+            </select>
+            <select {...register("attribute")}>
+                <option value="" disabled selected hidden>Attribute</option>
+                <option value="DARK">DARK</option>
+                <option value="DIVINE">DIVINE</option>
+                <option value="EARTH">EARTH</option>
+                <option value="FIRE">FIRE</option>
+                <option value="LIGHT">LIGHT</option>
+                <option value="WATER">WATER</option>
+                <option value="WIND">WIND</option>
+            </select>
             <input type="text" placeholder="Race" {...register("race")}/>
             <input type="text" placeholder="Archetype" {...register("archetype")}/>
             <input type="number" placeholder="Attack" {...register("atk")}/>
--- project/src/pages/SearchPage.js
+++ project/src/pages/SearchPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Search from '../components/Search.js'
+import {BsFillArrowLeftSquareFill, BsFillArrowRightSquareFill} from 'react-icons/bs'
 
 export default function SearchPage(){
     return(
@@ -9,8 +10,26 @@ export default function SearchPage(){
         </h2>
         <div className='grid-container'>
             <Search/>
-            <img id="Card_Image" src="Yugioh_Card_Back.png" alt="Yugioh Card Back"></img>
+            <div className="flip-card">
+                <div className="flip-card-inner">
+                    <div className="flip-card-front">
+                        <img id="Card_Image" src="Yugioh_Card_Back.png" alt="Yugioh Card Back"></img>
+                    </div>
+                    <div className="flip-card-back">
+                        <img id="Card_Image" src="Yugioh_Card_Back.png" alt="Yugioh Card Back"></img>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <BsFillArrowLeftSquareFill className = "arrow-icon"/>
+                <BsFillArrowRightSquareFill className = "arrow-icon"/>                
+            </div>
+            <div>
+                <BsFillArrowLeftSquareFill className = "arrow-icon"/>
+                <BsFillArrowRightSquareFill className = "arrow-icon"/>
+            </div>
         </div>
     </>
     )
 }
+
